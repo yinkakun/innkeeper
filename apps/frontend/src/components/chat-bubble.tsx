eed7a59fbc50d66c8157b30090789e6fca6426bc
yinkakun <yinkakun@gmail.com>
2024-08-01 20:19:27 +0100

    Add chat tail only if last by sender

--- apps/frontend/src/components/chat-bubble.tsx
+++ apps/frontend/src/components/chat-bubble.tsx
@@ -2,37 +2,45 @@ import React from 'react';
 import { cn } from '@/lib/utils';
 
 interface ChatBubbleProps {
-  message: string;
   isSender: boolean;
+  isLast?: boolean;
+  className?: string;
+  children?: React.ReactNode;
 }
 
-export const ChatBubble: React.FC<ChatBubbleProps> = ({ message, isSender }) => {
+export const ChatBubble: React.FC<ChatBubbleProps> = ({ isSender, children, className, isLast }) => {
   return (
     <div
-      className={cn('flex w-full text-sm', {
+      className={cn('my-px flex w-full text-sm', {
         'justify-end': isSender,
         'justify-start': !isSender,
       })}
     >
-      <div className="relative max-w-[80%]">
+      <div className="relative flex max-w-[80%]">
         <div
-          className={cn('flex w-fit text-pretty rounded-3xl px-4 py-2 tracking-tight text-opacity-90', {
-            'bg-orange-500 text-white': isSender,
-            'bg-gray-100 text-gray-700': !isSender,
-          })}
+          className={cn(
+            'flex w-fit flex-wrap overflow-auto whitespace-pre-line text-pretty rounded-3xl px-4 py-2 tracking-tight text-opacity-90',
+            {
+              'bg-orange-500 text-white': isSender,
+              'bg-gray-100 text-gray-700': !isSender,
+            },
+            className,
+          )}
         >
-          {message}
+          {children}
         </div>
         <div
           className={cn('absolute -bottom-[2px] z-20 h-[20px] w-[15px]', {
             '-left-[7px] rounded-br-[32px_28px] bg-gray-100': !isSender,
             '-right-[7px] rounded-bl-[32px_28px] bg-orange-500': isSender,
+            hidden: !isLast,
           })}
         />
         <div
           className={cn('absolute -bottom-[2px] z-20 h-[21px] w-[10px] bg-white', {
             '-left-[10px] rounded-br-[20px]': !isSender,
             '-right-[10px] rounded-bl-[20px]': isSender,
+            hidden: !isLast,
           })}
         />
       </div>
