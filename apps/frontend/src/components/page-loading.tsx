a7afadac82dba68e575df168ddac9b1dc49847a1
yinkakun <yinkakun@gmail.com>
2024-08-01 20:17:57 +0100

    Change loading component bg color to white

--- apps/frontend/src/components/page-loading.tsx
+++ apps/frontend/src/components/page-loading.tsx
@@ -1,8 +1,8 @@
 import { ThreeDotsScale } from 'react-svg-spinners';
 export const PageLoading = () => {
   return (
-    <div className="flex h-dvh items-center justify-center bg-orange-500 text-white">
-      <ThreeDotsScale width={50} height={50} color="#fff" />
+    <div className="flex h-dvh items-center justify-center bg-white text-orange-500">
+      <ThreeDotsScale width={50} height={50} color="#f54100" />
     </div>
   );
 };
