81680a2ae20e7343f9ecb0b659017f8cef4d1222
yinkakun <yinkakun@gmail.com>
2024-08-02 11:24:35 +0100

    Improve settings page UI

--- apps/frontend/src/pages/settings/index.tsx
+++ apps/frontend/src/pages/settings/index.tsx
@@ -10,7 +10,7 @@ import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Spinner } from '@/components/spinner';
 import { toast } from 'sonner';
-
+import { cn } from '@/lib/utils';
 const formSchema = z.object({
   name: z.string().min(1, 'First name is required').max(100, 'First name must be 100 characters or less'),
   promptFrequency: z.enum(['daily', 'weekly'], {
@@ -94,182 +94,210 @@ export const Settings = () => {
 
   return (
     <AppLayout>
-      <form className="flex grow flex-col gap-4" onSubmit={form.handleSubmit(onSubmit)}>
-        <div className="flex h-full w-full flex-col gap-6 rounded-3xl border border-neutral-200 bg-orange-50 bg-opacity-5 p-4 backdrop-blur-sm">
-          <div className="flex flex-col gap-1">
-            <label htmlFor="name" className="text-sm">
-              First Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              {...form.register('name')}
-              className="h-8 grow rounded-lg border border-orange-100 bg-orange-50 bg-opacity-20 px-2 py-1 text-xs placeholder:text-neutral-500 focus:border-orange-200 focus:bg-orange-50"
+      <div className="flex flex-col gap-4">
+        <form className="flex grow flex-col gap-4" onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex h-full w-full flex-col gap-6 rounded-3xl border border-border bg-white p-4">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="name" className="text-sm">
+                First Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                {...form.register('name')}
+                className="h-8 grow rounded-lg border border-border bg-gray-50 px-2 py-1 text-xs outline-none duration-200 placeholder:text-neutral-500 hover:border-orange-300 hover:bg-orange-50 focus:border-orange-300 focus:bg-orange-50"
+              />
+            </div>
+          </div>
+
+          <div className="flex h-full w-full flex-col gap-6 rounded-3xl border border-border bg-white p-4">
+            <Controller
+              name="promptFrequency"
+              control={form.control}
+              render={({ field }) => {
+                return (
+                  <RadioGroup className="flex w-full max-w-none shrink-0 flex-col gap-2" {...field}>
+                    <Label className="text-sm">Prompt Frequency</Label>
+                    <div className="grid w-full shrink-0 grid-cols-2 gap-4">
+                      {['Daily', 'Weekly'].map((item) => (
+                        <Radio
+                          className="group cursor-pointer rounded-lg border border-border bg-gray-50 p-2 py-1 text-sm text-gray-700 transition-colors duration-200 hover:border-orange-200 hover:bg-orange-50 hover:bg-opacity-50 data-[selected]:border-orange-300 data-[selected]:bg-orange-50"
+                          key={item}
+                          value={item.toLowerCase()}
+                        >
+                          {({ isSelected }) => (
+                            <div className="flex items-center gap-2 text-gray-800">
+                              <div
+                                className={cn('text-gray-800 duration-200 group-hover:text-orange-500', {
+                                  'text-orange-500': isSelected,
+                                })}
+                              >
+                                {isSelected ? <RadioButton weight="fill" /> : <Circle />}
+                              </div>
+
+                              <div>{item}</div>
+                            </div>
+                          )}
+                        </Radio>
+                      ))}
+                    </div>
+                  </RadioGroup>
+                );
+              }}
+            />
+
+            <Controller
+              name="promptPeriod"
+              control={form.control}
+              render={({ field }) => {
+                return (
+                  <RadioGroup className="flex flex-col gap-2" {...field}>
+                    <Label className="text-sm">Preferred Prompt Time</Label>
+                    <div className="grid w-full grid-cols-4 flex-col gap-2">
+                      {['Morning', 'Afternoon', 'Evening', 'Night'].map((time) => (
+                        <Radio
+                          key={time}
+                          value={time.toLowerCase()}
+                          className="group cursor-pointer rounded-lg border border-gray-200 bg-gray-50 p-2 py-1 text-sm text-gray-700 transition-colors duration-200 hover:border-orange-200 hover:bg-orange-50 data-[selected]:border-orange-400 data-[selected]:bg-orange-50"
+                        >
+                          {({ isSelected }) => (
+                            <div className="flex items-center gap-2">
+                              <div
+                                className={cn('text-gray-800 duration-200 group-hover:text-orange-500', {
+                                  'text-orange-500': isSelected,
+                                })}
+                              >
+                                {isSelected ? <RadioButton weight="fill" /> : <Circle />}
+                              </div>
+
+                              <div>{time}</div>
+                            </div>
+                          )}
+                        </Radio>
+                      ))}
+                    </div>
+                  </RadioGroup>
+                );
+              }}
+            />
+
+            <Controller
+              control={form.control}
+              name="enableEmailNotifications"
+              render={({ field }) => {
+                const id = React.useId();
+                return (
+                  <div className="flex items-center justify-between gap-2 rounded-xl border border-gray-200 bg-gray-50 bg-opacity-50 p-2 py-2">
+                    <label htmlFor={id} className="text-sm">
+                      Enable Email Notifications
+                    </label>
+
+                    <Switch id={id} checked={field.value} onCheckedChange={field.onChange} />
+                  </div>
+                );
+              }}
             />
           </div>
 
-          <Controller
-            name="promptFrequency"
-            control={form.control}
-            render={({ field }) => {
-              return (
-                <RadioGroup className="flex w-full max-w-none shrink-0 flex-col gap-2" {...field}>
-                  <Label className="text-sm">Prompt Frequency</Label>
-                  <div className="grid w-full shrink-0 grid-cols-2 gap-4">
-                    {['Daily', 'Weekly'].map((item) => (
-                      <Radio
-                        className="cursor-pointer rounded-lg border border-orange-100 bg-orange-50 bg-opacity-20 p-2 py-1 transition-colors duration-200 hover:bg-orange-50 hover:bg-opacity-20 data-[selected]:border-orange-400 data-[selected]:bg-orange-50 data-[selected]:text-neutral-700"
-                        key={item}
-                        value={item.toLowerCase()}
-                      >
-                        {({ isSelected }) => (
-                          <div className="flex items-center gap-2">
-                            {isSelected ? <RadioButton weight="fill" /> : <Circle />}
+          <div className="flex h-full w-full flex-col gap-6 rounded-3xl border border-border bg-white p-4">
+            <Controller
+              name="primaryGoal"
+              control={form.control}
+              render={({ field }) => {
+                return (
+                  <RadioGroup className="flex flex-col gap-2" {...field}>
+                    <Label className="text-sm capitalize">Primary Goal for journaling</Label>
+                    <div className="grid w-full grid-cols-3 flex-col gap-2">
+                      {GOALS.map((goal) => (
+                        <Radio
+                          key={goal.title}
+                          value={goal.title}
+                          className="group flex cursor-pointer flex-col gap-2 rounded-lg border border-gray-200 bg-gray-50 bg-opacity-50 p-3 text-gray-700 transition-colors duration-200 hover:border-orange-300 hover:bg-orange-50 data-[selected]:border-orange-400 data-[selected]:bg-orange-50"
+                        >
+                          {({ isSelected }) => (
+                            <React.Fragment>
+                              <div className="flex grow flex-col gap-2">
+                                <div
+                                  className={cn('text-gray-800 duration-200 group-hover:text-orange-500', {
+                                    'text-orange-500': isSelected,
+                                  })}
+                                >
+                                  {isSelected ? <RadioButton weight="fill" /> : <Circle />}
+                                </div>
+                                <h3 className="text-sm">{goal.title}</h3>
+                              </div>
+                              <p className="mt-auto text-[10px] text-gray-600">{goal.description}</p>
+                            </React.Fragment>
+                          )}
+                        </Radio>
+                      ))}
+                    </div>
+                  </RadioGroup>
+                );
+              }}
+            />
 
-                            <div>{item}</div>
-                          </div>
-                        )}
-                      </Radio>
-                    ))}
-                  </div>
-                </RadioGroup>
-              );
-            }}
-          />
+            <Controller
+              control={form.control}
+              name="promptTone"
+              render={({ field }) => {
+                return (
+                  <RadioGroup className="flex flex-col gap-2" {...field}>
+                    <Label className="text-sm">Preferred Prompt Tone</Label>
 
-          <Controller
-            name="promptPeriod"
-            control={form.control}
-            render={({ field }) => {
-              return (
-                <RadioGroup className="flex flex-col gap-2" {...field}>
-                  <Label className="text-sm">Preferred Prompt Time</Label>
-                  <div className="grid w-full grid-cols-4 flex-col gap-2">
-                    {['Morning', 'Afternoon', 'Evening', 'Night'].map((time) => (
-                      <Radio
-                        key={time}
-                        value={time.toLowerCase()}
-                        className="cursor-pointer rounded-lg border border-orange-100 bg-orange-50 bg-opacity-20 p-2 py-1 transition-colors duration-200 hover:bg-orange-50 hover:bg-opacity-20 data-[selected]:border-orange-400 data-[selected]:bg-orange-100 data-[selected]:text-neutral-700"
-                      >
-                        {({ isSelected }) => (
-                          <div className="flex items-center gap-2">
-                            {isSelected ? <RadioButton weight="fill" /> : <Circle />}
-                            <div>{time}</div>
-                          </div>
-                        )}
-                      </Radio>
-                    ))}
-                  </div>
-                </RadioGroup>
-              );
-            }}
-          />
-
-          <Controller
-            name="primaryGoal"
-            control={form.control}
-            render={({ field }) => {
-              return (
-                <RadioGroup className="flex flex-col gap-2" {...field}>
-                  <Label className="text-sm capitalize">Primary Goal for journaling</Label>
-                  <div className="grid w-full grid-cols-3 flex-col gap-2">
-                    {GOALS.map((goal) => (
-                      <Radio
-                        key={goal.title}
-                        value={goal.title}
-                        className="cursor-pointer rounded-lg border border-orange-100 bg-orange-50 bg-opacity-20 p-3 transition-colors duration-200 hover:bg-orange-50 hover:bg-opacity-20 data-[selected]:border-orange-400 data-[selected]:bg-orange-50 data-[selected]:text-neutral-700"
-                      >
-                        {({ isSelected }) => (
-                          <React.Fragment>
-                            <div className="flex flex-col gap-1">
-                              <div className="shrink-0">{isSelected ? <RadioButton weight="fill" /> : <Circle />}</div>
-                              <h3 className="text-xs">{goal.title}</h3>
-                            </div>
-                            <p className="text-[10px] text-neutral-600">{goal.description}</p>
-                          </React.Fragment>
-                        )}
-                      </Radio>
-                    ))}
-                  </div>
-                </RadioGroup>
-              );
-            }}
-          />
-
-          <Controller
-            control={form.control}
-            name="promptTone"
-            render={({ field }) => {
-              return (
-                <RadioGroup className="flex flex-col gap-2" {...field}>
-                  <Label className="text-sm">Preferred Prompt Tone</Label>
-
-                  <div className="grid w-full grid-cols-3 flex-col gap-2">
-                    {TONES.map((tone) => (
-                      <Radio
-                        key={tone.title}
-                        value={tone.title.toLowerCase()}
-                        className="flex cursor-pointer flex-col gap-2 rounded-lg border border-orange-100 bg-orange-50 bg-opacity-20 p-2 transition-colors duration-200 hover:bg-orange-50 hover:bg-opacity-20 data-[selected]:border-orange-400 data-[selected]:bg-orange-50 data-[selected]:text-neutral-700"
-                      >
-                        {({ isSelected }) => (
-                          <React.Fragment>
-                            <div className="flex items-center gap-2 text-neutral-900">
-                              <div className="shrink-0">{isSelected ? <RadioButton weight="fill" /> : <Circle />}</div>
-                              <h3 className="text-xs">{tone.title}</h3>
-                            </div>
-                            <p className="text-[10px] text-neutral-600">{tone.description}</p>
-                          </React.Fragment>
-                        )}
-                      </Radio>
-                    ))}
-                  </div>
-                </RadioGroup>
-              );
-            }}
-          />
-
-          <Controller
-            control={form.control}
-            name="enableEmailNotifications"
-            render={({ field }) => {
-              const id = React.useId();
-              return (
-                <div className="flex items-center justify-between gap-2 rounded-xl border border-orange-100 bg-orange-50 bg-opacity-20 p-2 py-2">
-                  <label htmlFor={id} className="text-sm">
-                    Enable Email Notifications
-                  </label>
-
-                  <Switch id={id} checked={field.value} onCheckedChange={field.onChange} />
-                </div>
-              );
-            }}
-          />
+                    <div className="grid w-full grid-cols-3 flex-col gap-2">
+                      {TONES.map((tone) => (
+                        <Radio
+                          key={tone.title}
+                          value={tone.title.toLowerCase()}
+                          className="group flex cursor-pointer flex-col gap-2 rounded-lg border border-gray-200 bg-gray-50 bg-opacity-50 p-2 text-neutral-700 transition-colors duration-200 hover:bg-gray-50 data-[selected]:border-orange-400 data-[selected]:bg-orange-50"
+                        >
+                          {({ isSelected }) => (
+                            <React.Fragment>
+                              <div className="flex items-center gap-2 text-neutral-900">
+                                <div
+                                  className={cn('text-gray-800 duration-200 group-hover:text-orange-500', {
+                                    'text-orange-500': isSelected,
+                                  })}
+                                >
+                                  {isSelected ? <RadioButton weight="fill" /> : <Circle />}
+                                </div>
+                                <h3 className="text-sm">{tone.title}</h3>
+                              </div>
+                              <p className="text-[10px] text-neutral-600">{tone.description}</p>
+                            </React.Fragment>
+                          )}
+                        </Radio>
+                      ))}
+                    </div>
+                  </RadioGroup>
+                );
+              }}
+            />
+          </div>
 
           <div className="flex items-center justify-between gap-4">
             <button className="ml-auto flex h-8 w-[120px] items-center justify-center rounded-lg bg-[#FF4800] bg-gradient-to-r from-[#FF5C0A] to-[#F54100] py-1 text-sm font-medium text-neutral-50 duration-200">
               <span>{mutation.isPending ? <Spinner /> : 'Save Changes'}</span>
             </button>
           </div>
-        </div>
+        </form>
+        <div className="divide-y-gray-200 flex flex-col divide-y overflow-hidden rounded-xl border border-border">
+          <button type="button" className="group w-full px-6 py-2 text-left text-sm duration-200 hover:bg-gray-50">
+            <span>Logout</span>
+          </button>
 
-        <div className="flex flex-col divide-y rounded-3xl border">
-          <div className="px-6 py-2">
-            <button className="text-xs">
-              <span>Logout</span>
-            </button>
-          </div>
-          <div className="p-2 px-4">
-            <button
-              onClick={() => {
-                alert('Coming soon');
-              }}
-              className="text-xs text-red-600"
-            >
-              Delete Account
-            </button>
-          </div>
+          <button
+            type="button"
+            onClick={() => {
+              alert('Coming soon');
+            }}
+            className="w-full p-2 px-4 text-left text-sm text-red-600 duration-200 hover:bg-gray-50"
+          >
+            Delete Account
+          </button>
         </div>
-      </form>
+      </div>
     </AppLayout>
   );
 };
