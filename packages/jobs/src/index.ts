b9f23208f80190422bdc174bd16f949a903aeda3
yinkakun <yinkakun@gmail.com>
2024-08-18 09:52:42 +0100

    Temporarily remove disable inactive users job

--- packages/jobs/src/index.ts
+++ packages/jobs/src/index.ts
@@ -1,4 +1,3 @@
 export * from './trigger/save-journal-entry';
 export * from './trigger/send-welcome-message';
 export * from './trigger/send-journal-prompts';
-export * from './trigger/disable-inactive-users';
