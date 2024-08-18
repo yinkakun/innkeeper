35f65da9bb73664e27df736b54c72b703c9ec69f
yinkakun <yinkakun@gmail.com>
2024-08-18 06:16:11 +0100

    Update context menu stylings

--- apps/frontend/src/components/context-menu.tsx
+++ apps/frontend/src/components/context-menu.tsx
@@ -3,6 +3,7 @@ import { cn } from '@/lib/utils';
 import * as ContextMenuPrimitive from '@radix-ui/react-context-menu';
 
 export const ContextMenu = ContextMenuPrimitive.Root;
+export const ContextMenuPortal = ContextMenuPrimitive.Portal;
 export const ContextMenuTrigger = ContextMenuPrimitive.Trigger;
 
 export const ContextMenuContent = React.forwardRef<
@@ -13,7 +14,7 @@ export const ContextMenuContent = React.forwardRef<
     <ContextMenuPrimitive.Content
       ref={ref}
       className={cn(
-        'animate-in fade-in-80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[10rem] overflow-hidden rounded-xl border border-border bg-popover p-0 text-popover-foreground shadow-md',
+        'animate-in fade-in-80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[10rem] overflow-hidden rounded-xl border border-border border-orange-50 bg-white bg-opacity-50 p-1 text-popover-foreground shadow-md backdrop-blur-xl',
         className,
       )}
       {...props}
@@ -31,7 +32,7 @@ export const ContextMenuItem = React.forwardRef<
   <ContextMenuPrimitive.Item
     ref={ref}
     className={cn(
-      'relative flex cursor-default select-none items-center gap-2 rounded-sm bg-orange-50/10 px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      'relative flex cursor-default select-none items-center gap-2 rounded-lg bg-orange-50/10 px-2 py-1 text-sm outline-none focus:bg-orange-50 focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
       inset && 'pl-8',
       className,
     )}
@@ -44,6 +45,6 @@ export const ContextMenuSeparator = React.forwardRef<
   React.ElementRef<typeof ContextMenuPrimitive.Separator>,
   React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Separator>
 >(({ className, ...props }, ref) => (
-  <ContextMenuPrimitive.Separator ref={ref} className={cn('-mx-1 my-0 h-px bg-border', className)} {...props} />
+  <ContextMenuPrimitive.Separator ref={ref} className={cn('-mx-1 my-1 h-px bg-orange-50', className)} {...props} />
 ));
 ContextMenuSeparator.displayName = ContextMenuPrimitive.Separator.displayName;
