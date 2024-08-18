5fa5cd68f19b1825a8cd08cd55329550028b11f1
yinkakun <yinkakun@gmail.com>
2024-08-18 09:54:09 +0100

    Sends user first prompt after onboarding

--- packages/jobs/src/trigger/send-welcome-message.ts
+++ packages/jobs/src/trigger/send-welcome-message.ts
@@ -1,14 +1,46 @@
-import { task } from '@trigger.dev/sdk/v3';
-import { email } from '../lib';
+import { email, db, llm } from '../lib';
+import { task, retry } from '@trigger.dev/sdk/v3';
 
 interface SendWelcomeEmailPayload {
-  name: string;
   email: string;
+  userId: string;
+  name: string | null;
+  promptTone: string | null;
+  primaryGoal: string | null;
 }
 
 export const sendWelcomeEmail = task({
   id: 'send-welcome-email',
   run: async (payload: SendWelcomeEmailPayload) => {
-    await email.welcome({ name: payload.name, to: payload.email });
+    const response = await retry.onThrow(async () => {
+      return await llm.generatePrompt({
+        tone: payload.promptTone,
+        goal: payload.primaryGoal,
+      });
+    }, {});
+
+    const prompt = response.content[0]?.type === 'text' ? response.content[0].text : '';
+
+    const createPromptResponse = await db.createPrompt({
+      prompt,
+      userId: payload.userId,
+      userEmail: payload.email,
+    });
+
+    if (!createPromptResponse) {
+      throw new Error('Failed to create prompt');
+    }
+
+    const promptNumber = createPromptResponse.promptNumber;
+
+    await retry.onThrow(async () => {
+      return await email.sendPrompt({
+        prompt,
+        to: payload.email,
+        name: payload.name ?? 'Anon',
+        senderUsername: 'innkeeper-staging',
+        subject: `Innkeeper Journal Prompt #${promptNumber}`,
+      });
+    }, {});
   },
 });
