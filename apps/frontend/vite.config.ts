450c52e1dc515fe22b6c28b81ae230417e7426f7
yinkakun <yinkakun@gmail.com>
2024-08-29 15:35:00 +0100

    Add image optimization tooling
    This will reduce image sizes,
    leading to improved page load times and overall
    performance

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImageOptimizer({})],
   resolve: {
     alias: {
       '@': '/src',
